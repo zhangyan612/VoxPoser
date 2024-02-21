f9327d9e6f6d6b7a7186588efd7dd46de4386f00
Yan Zhang <zhangyan612@gmail.com>
2024-02-21 00:37:21 -0600

    update cache

--- src/LLM_cache.py
+++ src/LLM_cache.py
@@ -67,22 +67,31 @@ if __name__ == '__main__':
     #     print(key)
     #     print(value)
 
-    # for filename in os.listdir('cache'):
-    #     with open(os.path.join('cache', filename), 'rb') as file:
-    #         key, value = pickle.load(file)
-    #         print(filename)
-    #         print(value)
+    # get all cache
+    for filename in os.listdir('cache'):
+        with open(os.path.join('cache', filename), 'rb') as file:
+            key, value = pickle.load(file)
+            print(filename)
+
+            for i in key['messages']:
+                print(i['content'])
+            print('####################################')
+            print(value)
 
     # check single file
-    with open(os.path.join('cache', 'c3a2e21bb61b0b606216af8f5199b8cf3b76d974.pkl'), 'rb') as file:
-        key, value = pickle.load(file)
-        # print(key)
-        # print(key['messages'])
-        for i in key['messages']:
-            print(i['content'])
-        # value = value.replace("\\", "")
-        print(value)
+    # with open(os.path.join('cache', 'c3a2e21bb61b0b606216af8f5199b8cf3b76d974.pkl'), 'rb') as file:
+    #     key, value = pickle.load(file)
+    #     # print(key)
+    #     # print(key['messages'])
+    #     for i in key['messages']:
+    #         print(i['content'])
+    #     # value = value.replace("\\", "")
+    #     print('####################################')
+    #     print(value)
 
-    # update single file
-    # with open(os.path.join('cache', '6f11d122d7445d0f9b9e66273bb8de85f527f84c.pkl'), 'wb') as file:
-    #     pickle.dump((key, value), file)
+    # get updated value from gpt
+    # gptValue = 'composer("grasp the rubbish")\ncomposer("back to default pose")\ncomposer("move to the center of the opening of the bin")\ncomposer("open gripper")\ncomposer("back to default pose")'
+    
+    # # update single file
+    # with open(os.path.join('cache', 'c3a2e21bb61b0b606216af8f5199b8cf3b76d974.pkl'), 'wb') as file:
+    #     pickle.dump((key, gptValue), file)
